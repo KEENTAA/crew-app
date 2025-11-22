17c24e69d7e446b9e5240c796cb1db5586195f13
KEENTAA <ramiro.quenta@ucb.edu.bo>
2025-11-22 12:56:45 -0400

    feat: Add secure environment variables

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,19 +5,17 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBdNq6fZdWXMINJZojiKdMdtSNG-EZCk6M",
-  authDomain: "crew-crowdfunding-simulado.firebaseapp.com",
-  projectId: "crew-crowdfunding-simulado",
-  storageBucket: "crew-crowdfunding-simulado.appspot.com",
-  messagingSenderId: "752583559935",
-  appId: "1:752583559935:web:08ce00c0853c83368c1aca"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-// Forzar el bucket correcto de Storage
-export const storage = getStorage(app, "gs://crew-crowdfunding-simulado.appspot.com");
-
+export const storage = getStorage(app);
 export default app;
